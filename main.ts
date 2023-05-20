a4ad7e0ac1f7fddf7e2d6021dd48f841a8aa5368
flower <zhz970809@163.com>
2023-05-20 12:15:01 +0800

    学习ts

---
+++ main.ts
@@ -0,0 +1,20 @@
+// enum color {
+//   aa,
+//   no = 'no',
+//   yes = 1
+// }
+// console.log(color);
+
+// enum Color1{
+//   red = 1,
+//   green,
+//   blue
+// }
+// console.log(Color1); //1
+var a = new Boolean(false)
+console.log( a && true);
+
+var b = a && true
+console.log(b);
+b = a && true
+console.log(b);
